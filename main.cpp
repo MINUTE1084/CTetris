873ee5969013a24b5302e586bb49f4b04a8d7f75
swpro1004 <swpro1004@gmail.com>
2019-04-17 19:17:12 +0900

    수정
    주석

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 
 int main() {
 	srand((unsigned)time(NULL));
@@ -8,16 +8,16 @@ int main() {
 	T->title();
 
 	while (1) {
-		for (int i = 0; i < 5; i++) { // Can get key inpur 5 times
+		for (int i = 0; i < 5; i++) { // 최대 5회의 키 입력
 			T->keyCheck();
 			T->drawGameboard();
 			Sleep(T->getSpeed());
-			if (T->getEvent(0) && T->checkCollision(T->getPos(0), T->getPos(1), T->getRotation())) Sleep(100); // if block can't move down, Delay
+			if (T->getEvent(0) && T->checkCollision(T->getPos(0), T->getPos(1), T->getRotation())) Sleep(100); // 블럭 이동이 불가능 하다면 잠시 멈춤
 		}
-		T->dropBlock();
+		T->dropBlock(); 
 		T->checkLvUp();
-		if (T->checkGameOver()) break; // if currently game over, break while (end game)
-		if (T->getEvent(1)) T->generateNewBlock(); // if need new block, generate block
+		if (T->checkGameOver()) break; // 게임 오버 상태일 경우 게임을 진행하지 않음
+		if (T->getEvent(1)) T->generateNewBlock(); // 블럭이 놓여진 상태일 때, 새 블럭 생성
 	}
 	delete T;
 	while (1) {
